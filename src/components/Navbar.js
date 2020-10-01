d8a19da0a7571fa3c8f31272dc11928ee367a66a
mfayaq <mohdfayaq@gmail.com>
2020-10-01 22:51:33 +0530

    Navbar Conntected to Router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Layout, Menu } from "antd";
 
 import "../App.css";
@@ -8,23 +8,26 @@ const { Header, Footer } = Layout;
 
 class Navbar extends Component {
 	render() {
+		let { pathname } = this.props.history.location;
+		let pathName = pathname === "/" ? "home" : pathname.slice(1);
+		console.log(pathName);
 		return (
 			<div className="nav-container">
 				<Layout>
 					<Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
 						<div className="logo" />
-						<Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-							<Menu.Item key="1">
+						<Menu theme="dark" mode="horizontal" selectedKeys={[pathName]}>
+							<Menu.Item key="home">
 								<Link to="/" className="">
 									Home
 								</Link>
 							</Menu.Item>
-							<Menu.Item key="2">
+							<Menu.Item key="login">
 								<Link to="/login" className="">
 									Login
 								</Link>
 							</Menu.Item>
-							<Menu.Item key="3">
+							<Menu.Item key="signup">
 								<Link to="/signup" className="">
 									Signup
 								</Link>
@@ -33,10 +36,9 @@ class Navbar extends Component {
 					</Header>
 					<Footer style={{ textAlign: "center" }}></Footer>
 				</Layout>
-				,
 			</div>
 		);
 	}
 }
 
-export default Navbar;
+export default withRouter(Navbar);
